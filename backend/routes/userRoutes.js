6d0e0305f4ef86083483f1970d6b8c3259ca3130
abrarnabi <117633785+Abrar-Nabi@users.noreply.github.com>
2025-04-30 11:52:37 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,7 +6,8 @@ const router = express.Router();
 // GET all users
 router.get("/", async (req, res) => {
   try {
-    const users = await User.find({}, "name email _id"); // Fetch only necessary fields
+    const users = await User.find({}, "name email _id role");
+    // Fetch only necessary fields
     res.json(users);
   } catch (err) {
     res.status(500).json({ error: "Failed to fetch users" });
